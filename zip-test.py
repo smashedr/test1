4018ba8cb71f389f68c88d8ac155642145fd9e3b
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-17 16:15:01 -0700

    Drone

--- zip-test.py
+++ zip-test.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import psycopg2
 import requests
 
@@ -5,23 +8,30 @@ pg_data = {
     'host': 'postgres',
     'database': 'postgres',
     'user': 'postgres',
-    'password': 'postgres'
+    'password': 'postgres',
 }
-
 query = 'SELECT "token" FROM public."User" WHERE id=1 ORDER BY id ASC;'
 
-data = {"username": "administrator", "password": "password", "code": None}
-url = 'http://zipline:3003/api/auth/login'
-r = requests.post(url, json=data)
-print(r.status_code)
-# print(r.text)
+zip_data = {
+    'username': 'administrator',
+    'password': 'password',
+    'code': None,
+}
+zip_url = 'http://zipline:3003/api/auth/login'
+
+r = requests.post(zip_url, json=zip_data)
+r.raise_for_status()
+# print(r.cookies)
+
+
 with psycopg2.connect(**pg_data) as conn:
     with conn.cursor() as cur:
-
         cur.execute(query)
         result = cur.fetchone()
-if result:
-    print(f'Token: {result[0]}')
-else:
-    print('NO RESULTS:')
-    print(result)
+
+if not result:
+    print('No Results')
+    sys.exit(1)
+
+print(f'{result[0]}')
+os.environ['ZIPLINE_TOKEN'] = result[0]
