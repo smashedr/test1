b7cdee9fc703d111551b0c29c747a14755baad8d
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-17 16:25:00 -0700

    Drone

--- zip-test.py
+++ zip-test.py
@@ -1,8 +1,9 @@
 import os
 import sys
-
 import psycopg2
 import requests
+
+query = 'SELECT "token" FROM public."User" WHERE id=1 ORDER BY id ASC;'
 
 pg_data = {
     'host': 'postgres',
@@ -10,28 +11,31 @@ pg_data = {
     'user': 'postgres',
     'password': 'postgres',
 }
-query = 'SELECT "token" FROM public."User" WHERE id=1 ORDER BY id ASC;'
 
 zip_data = {
     'username': 'administrator',
     'password': 'password',
     'code': None,
 }
-zip_url = 'http://zipline:3003/api/auth/login'
 
+zip_uri = os.environ.get('ZIPLINE_URL', 'http://zipline:3003').rstrip('/')
+zip_url = f'{zip_uri}/api/auth/login'
+
+# log in to create account (and get cookie)
 r = requests.post(zip_url, json=zip_data)
 r.raise_for_status()
-# print(r.cookies)
 
-
+# get token (or could use cookie, not tested)
 with psycopg2.connect(**pg_data) as conn:
     with conn.cursor() as cur:
         cur.execute(query)
         result = cur.fetchone()
 
+# exit with error if no results
 if not result:
     print('No Results')
     sys.exit(1)
 
+# export environment variable
 print(f'{result[0]}')
 os.environ['ZIPLINE_TOKEN'] = result[0]
